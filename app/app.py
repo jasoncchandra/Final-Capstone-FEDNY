50712655cc5764d85637d1f3cb057e11a136f78c
jasoncchandra <141464490+jasoncchandra@users.noreply.github.com>
2024-07-15 15:40:19 -0500

    assigned

--- app/app.py
+++ app/app.py
@@ -20,19 +20,23 @@ nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
 
+# Define the return_self function
+def return_self(x):
+    return x
+
 # Check if the files exist in the current directory
 st.write("Current directory:", os.getcwd())
 st.write("Files in current directory:", os.listdir(os.getcwd()))
 
 # Load the vectorizer and LDA model
 try:
-    vectorizer = joblib.load('vectorizer_v8.pkl')
+    vectorizer = joblib.load('count_vectorizer.pkl')
     st.success("Vectorizer loaded successfully.")
 except FileNotFoundError as e:
     st.error(f"Vectorizer file not found: {e}")
 
 try:
-    lda_model = joblib.load('lda_model_8.pkl')
+    lda_model = joblib.load('lda_model_6_topics.pkl')
     st.success("LDA model loaded successfully.")
 except FileNotFoundError as e:
     st.error(f"LDA model file not found: {e}")
@@ -72,8 +76,8 @@ def plot_topic_distribution(topic_distribution, topic_dict):
 
     fig, ax = plt.subplots()
     sns.barplot(x=scores, y=topics, ax=ax)
-    ax.set_title('Topic Distribution')
-    ax.set_xlabel('Average Score')
+    ax.set_title('Average Topic Distribution (Weighted)')
+    ax.set_xlabel('Average Topic Distribution (Weighted)')
     ax.set_ylabel('Topics')
     st.pyplot(fig)
 
@@ -81,7 +85,7 @@ def plot_word_clouds(lda_model, vectorizer, topic_dict):
     from wordcloud import WordCloud
 
     feature_names = vectorizer.get_feature_names_out()
-    fig, axes = plt.subplots(2, 4, figsize=(20, 10), sharex=True, sharey=True)
+    fig, axes = plt.subplots(2, 3, figsize=(15, 10), sharex=True, sharey=True)
 
     for idx, topic in enumerate(topic_dict.keys()):
         wordcloud = WordCloud(
@@ -93,23 +97,11 @@ def plot_word_clouds(lda_model, vectorizer, topic_dict):
         ).generate_from_frequencies(
             {feature_names[i]: lda_model.components_[topic, i] for i in lda_model.components_[topic].argsort()[:-11:-1]}
         )
-        ax = axes[idx // 4, idx % 4]
+        ax = axes[idx // 3, idx % 3]
         ax.imshow(wordcloud, interpolation='bilinear')
         ax.set_title(topic_dict[topic])
         ax.axis('off')
 
-    st.pyplot(fig)
-
-def plot_coherence_scores(coherence_scores):
-    num_topics = list(coherence_scores.keys())
-    scores = list(coherence_scores.values())
-
-    fig, ax = plt.subplots()
-    ax.plot(num_topics, scores, marker='o')
-    ax.set_title('Coherence Scores by Number of Topics')
-    ax.set_xlabel('Number of Topics')
-    ax.set_ylabel('Coherence Score')
-    ax.grid(True)
     st.pyplot(fig)
 
 def analyze_sentiment(text):
@@ -153,7 +145,7 @@ def display_key_sentences(sentences, topic_distributions, topic_dict, top_n=5):
                 for sentence, score in sentences:
                     st.write(f"{sentence} (Score: {score:.4f})")
             else:
-                st.write("No sentences found for this topic.")
+                st.write("No sentences found where this topic is dominant.")
 
 def search_sentences(sentences, query):
     results = [sentence for sentence in sentences if query.lower() in sentence.lower()]
@@ -176,54 +168,51 @@ if page == 'Introduction':
 # Training Process Page
 elif page == 'Training Process':
     st.header('Training Process')
-    st.write('We experimented with different numbers of topics to find the most relevant ones. It was a judgment call to select the optimal number of topics, balancing the need for distinct features with the desire to avoid having too many topics. Here are the coherence scores for different numbers of topics:')
+    st.write('In this project, we performed topic modeling on Federal Reserve meeting minutes to extract key themes. '
+             'We used Latent Dirichlet Allocation (LDA) for topic modeling. Here are the identified topics and their descriptions:')
 
-    coherence_scores = {
-        3: 0.09004151822108149,
-        4: 0.0046645116832365555,
-        5: -0.0045957082288738925,
-        6: 0.0017441943397127069,
-        7: 0.008529514229556334,
-        8: 0.01629442206421782,
-        9: -0.011536801725663837,
-        10: -0.1372346118130309,
-        11: -0.1595829856257721,
-        12: -0.15113393781221862,
-        13: -0.1431670808158615,
-        14: -0.15929615987573326,
-        15: -0.15891563765618963,
-        16: -0.1583665063308525,
-        17: -0.15529978708000322,
-        18: -0.15851683435827796,
-        19: -0.1515668846175406
+    topic_descriptions = {
+        "Topic 0": "Quarterly Economic Performance and Loans\n\n"
+                   "Representation: Discussions about quarterly economic performance, loan activities, and credit markets.\n"
+                   "Justification: The FOMC reviews quarterly economic data and considers the state of the credit markets for economic stability and growth.",
+
+        "Topic 1": "Inflation and Monetary Policy\n\n"
+                   "Representation: Focus on inflation and the committee's policy decisions.\n"
+                   "Justification: Inflation control is a primary objective of the FOMC, involving policy measures to manage inflation.",
+
+        "Topic 2": "Price Levels and Labor Market\n\n"
+                   "Representation: Covers price levels, inflation measurements, and labor market conditions.\n"
+                   "Justification: Monitoring price levels and labor market conditions helps assess economic health and inflationary pressures.",
+
+        "Topic 3": "Economic Growth and Forecasts\n\n"
+                   "Representation: Related to overall economic growth, forecasts, and projections.\n"
+                   "Justification: Economic forecasts and growth projections are essential for planning and adjusting monetary policy.",
+
+        "Topic 4": "Market Conditions and Financial Stability\n\n"
+                   "Representation: Focus on market conditions and financial stability.\n"
+                   "Justification: Ensuring stable market conditions and a sound financial system is a core responsibility of the FOMC.",
+
+        "Topic 5": "Business Activities and Consumer Spending\n\n"
+                   "Representation: Covers business activities and consumer spending.\n"
+                   "Justification: Understanding business activities and consumer spending patterns helps gauge economic momentum and support sustainable growth."
     }
 
-    # Plot coherence scores
-    st.subheader('Coherence Scores Graph')
-    plot_coherence_scores(coherence_scores)
-
-    st.write("Based on the coherence scores, we decided to use 8 topics. Here are the topics and their top words:")
-
-    topics_8 = {
-        "Topic 0": ['payment', 'bank', 'reserve', 'service', 'federal', 'financial', 'check', 'currency', 'electronic', 'consumer'],
-        "Topic 1": ['financial', 'crisis', 'liquidity', 'capital', 'stress', 'firm', 'asset', 'fund', 'requirement', 'regulation'],
-        "Topic 2": ['friedman', 'gold', 'schwartz', 'hayek', 'carolina', '1929', 'charlotte', 'north', '1928', '1931'],
-        "Topic 3": ['rate', 'price', 'economy', 'growth', 'policy', 'inflation', 'economic', 'productivity', 'country', 'capital'],
-        "Topic 4": ['policy', 'financial', 'bank', 'reserve', 'federal', 'community', 'monetary', 'inflation', 'data', 'rate'],
-        "Topic 5": ['inflation', 'price', 'growth', 'rate', 'economic', 'quarter', 'committee', 'policy', 'increase', 'recent'],
-        "Topic 6": ['bank', 'risk', 'financial', 'banking', 'capital', 'management', 'institution', 'credit', 'organization', 'business'],
-        "Topic 7": ['rate', 'participant', 'inflation', 'committee', 'economic', 'federal', 'policy', 'term', 'condition', 'security']
+    for topic, description in topic_descriptions.items():
+        st.subheader(topic)
+        st.write(description)
+        
+    # Display the topic-word table as a DataFrame
+    topic_word_data = {
+        "Topic 0": ["quarter", "loan", "increas", "real", "remain", "declin", "continu", "credit", "sale", "consum"],
+        "Topic 1": ["inflat", "committe", "particip", "polici", "would", "econom", "member", "rate", "feder", "risk"],
+        "Topic 2": ["inflat", "price", "labor", "month", "rate", "measur", "percent", "year", "increas", "remain"],
+        "Topic 3": ["growth", "econom", "project", "year", "quarter", "economi", "rate", "forecast", "gdp", "staff"],
+        "Topic 4": ["market", "period", "rate", "intermeet", "bank", "financi", "treasuri", "fund", "secur", "remain"],
+        "Topic 5": ["particip", "busi", "spend", "price", "growth", "note", "continu", "recent", "sector", "market"]
     }
 
-    for topic, words in topics_8.items():
-        st.write(f"{topic}: {', '.join(words)}")
-
-    # Plot word clouds for each topic
-    st.subheader('Word Clouds for Each Topic')
-    topic_dict = {0: 'Payment Systems and Services', 1: 'Financial Crisis and Liquidity', 2: 'Historical Economic Figures',
-                  3: 'Economic Growth and Policy', 4: 'Monetary Policy and Inflation', 5: 'Inflation and Economic Growth',
-                  6: 'Banking and Risk Management', 7: 'Federal Reserve and Monetary Policy'}
-    plot_word_clouds(lda_model, vectorizer, topic_dict)
+    topic_word_df = pd.DataFrame(topic_word_data)
+    st.dataframe(topic_word_df)
 
 # Topic Modeling & Sentiment Analysis Page
 elif page == 'Topic Modeling & Sentiment Analysis':
@@ -262,23 +251,18 @@ elif page == 'Topic Modeling & Sentiment Analysis':
             progress_bar.progress(80)
 
             # Display topic distribution
-            st.subheader('Sentence-Level Topic Distribution')
+            st.subheader('Dominant Topic Sentence-Level Distribution')
 
             topic_dict = {
-                0: 'Payment Systems and Services',
-                1: 'Financial Crisis and Liquidity',
-                2: 'Historical Economic Figures',
-                3: 'Economic Growth and Policy',
-                4: 'Monetary Policy and Inflation',
-                5: 'Inflation and Economic Growth',
-                6: 'Banking and Risk Management',
-                7: 'Federal Reserve and Monetary Policy'
+                0: 'Quarterly Economic Performance and Loans',
+                1: 'Inflation and Monetary Policy',
+                2: 'Price Levels and Labor Market',
+                3: 'Economic Growth and Forecasts',
+                4: 'Market Conditions and Financial Stability',
+                5: 'Business Activities and Consumer Spending'
             }
 
-            for idx in range(8):  # Ensure all topics are displayed
-                count = topic_counts.get(idx, 0)
-                st.write(f"Topic {idx} ({topic_dict.get(idx, 'Unknown')}): {count:.2%}")
-
+            # Calculate and display the topic percentages based on average topic distribution
             # Plot topic distribution
             plot_topic_distribution(topic_distributions, topic_dict)
             progress_bar.progress(90)
@@ -296,7 +280,7 @@ elif page == 'Topic Modeling & Sentiment Analysis':
                     sentiments.append(sentiment)
                     sentiment_progress.progress((i + 1) / len(sentences))
 
-                avg_sentiment_by_topic = aggregate_sentiment_by_topic(sentences, topic_distributions, sentiments, num_topics=8)
+                avg_sentiment_by_topic = aggregate_sentiment_by_topic(sentences, topic_distributions, sentiments, num_topics=6)
 
                 # Display average sentiment by topic
                 st.subheader('Sentiment Analysis by Topic')
